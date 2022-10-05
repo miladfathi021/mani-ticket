7d0eebc60bb6181fb451864ee39d8240d84d3289
Milad <miladfathi021@gmail.com>
2022-10-05 23:58:43 +0330

    Create Hall model with migration file

---
+++ database/migrations/2022_08_24_193802_create_halls_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHallsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('halls', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')
+                ->constrained('users')
+                ->cascadeOnDelete();
+
+            $table->string('name');
+
+            $table->foreignId('parent_id')
+                ->nullable()
+                ->constrained('halls')
+                ->cascadeOnDelete();
+
+            $table->string('description')->nullable();
+            $table->string('address')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('halls');
+    }
+}
