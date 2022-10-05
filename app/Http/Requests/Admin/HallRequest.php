392fcb1f0ed54875df72cabdcdee66cfd44dfaaa
Milad <miladfathi021@gmail.com>
2022-10-05 23:57:33 +0330

    Create hall controller

---
+++ app/Http/Requests/Admin/HallRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests\Admin;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class HallRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'bail|required|string|min:3|max:255',
+            'description' => 'bail|sometimes|nullable|string|min:3',
+            'address' => 'bail|sometimes|nullable|string|min:6',
+            'floors' => 'bail|present|array',
+            'floors.*' => 'bail|sometimes|array',
+            'floors.*.name' => 'bail|required|string|min:3|max:255',
+            'floors.*.description' => 'bail|sometimes|nullable|string|min:3',
+        ];
+    }
+}
