2219a6566873e2a09322f5205c86dabd04755729
Milad <miladfathi021@gmail.com>
2022-11-04 22:48:56 +0330

    Created complex request

---
+++ app/Http/Requests/Admin/ComplexRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests\Admin;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ComplexRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'bail|required|string|min:3|max:255',
+            'description' => 'bail|sometimes|nullable|string|min:3',
+            'address' => 'bail|sometimes|nullable|string|min:6',
+            'halls' => 'bail|present|array',
+            'halls.*' => 'bail|sometimes|array',
+            'halls.*.name' => 'bail|required|string|min:3|max:255',
+            'halls.*.description' => 'bail|sometimes|nullable|string|min:3',
+        ];
+    }
+}
