cdefb6bc1d87ac8a132f678a2e2edc75235d7c58
Milad <miladfathi021@gmail.com>
2022-11-07 20:36:39 +0330

    Created repository service provider

--- app/Providers/RepositoriesServiceProvider.php
+++ app/Providers/RepositoriesServiceProvider.php
@@ -2,8 +2,16 @@
 
 namespace App\Providers;
 
+use App\Repositories\ComplexRepository\ComplexRepositoryInterface;
+use App\Repositories\ComplexRepository\EloquentComplexRepository;
+use App\Repositories\EventRepository\EloquentEventRepository;
+use App\Repositories\EventRepository\EventRepositoryInterface;
 use App\Repositories\HallRepository\EloquentHallRepository;
 use App\Repositories\HallRepository\HallRepositoryInterface;
+use App\Repositories\SeatRepository\EloquentSeatRepository;
+use App\Repositories\SeatRepository\SeatRepositoryInterface;
+use App\Repositories\SectionRepository\EloquentSectionRepository;
+use App\Repositories\SectionRepository\SectionRepositoryInterface;
 use Illuminate\Support\ServiceProvider;
 
 class RepositoriesServiceProvider extends ServiceProvider
@@ -25,6 +33,10 @@ class RepositoriesServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        $this->app->bind(ComplexRepositoryInterface::class, EloquentComplexRepository::class);
         $this->app->bind(HallRepositoryInterface::class, EloquentHallRepository::class);
+        $this->app->bind(SectionRepositoryInterface::class, EloquentSectionRepository::class);
+        $this->app->bind(SeatRepositoryInterface::class, EloquentSeatRepository::class);
+        $this->app->bind(EventRepositoryInterface::class, EloquentEventRepository::class);
     }
 }
