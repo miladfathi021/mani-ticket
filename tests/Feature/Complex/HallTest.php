2a858f6bc8289658bb1855561c2ba8ef8ec90270
Milad <miladfathi021@gmail.com>
2022-11-07 20:29:30 +0330

    Created hall test

---
+++ tests/Feature/Complex/HallTest.php
@@ -0,0 +1,168 @@
+<?php
+
+namespace Tests\Feature\Complex;
+
+use App\Models\Complex;
+use App\Models\Hall;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class HallTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test **/
+    public function unauthenticated_user_can_not_create_a_new_hall()
+    {
+        $complex = Complex::factory()->create();
+        $data = [
+            'name' => 'Milad Hall',
+            'description' => 'This is a description',
+            'complex_id' => $complex->id
+        ];
+
+        $this->assertDatabaseCount('halls', 0);
+
+        $this->postJson(route('halls.store'), $data)
+            ->assertStatus(401);
+
+        $this->assertDatabaseCount('halls', 0);
+    }
+
+    /** @test **/
+    public function admin_can_create_a_new_hall()
+    {
+        $this->withoutExceptionHandling();
+        $this->singIn();
+
+        $complex = Complex::factory()->create([
+            'user_id' => auth()->id()
+        ]);
+
+        $data = [
+            'name' => 'Milad Hall',
+            'description' => 'This is a description',
+            'complex_id' => $complex->id
+        ];
+
+        $this->assertDatabaseCount('halls', 0);
+
+        $this->postJson(route('halls.store'), $data)
+            ->assertStatus(200);
+
+        $this->assertDatabaseCount('halls', 1);
+        $this->assertDatabaseHas('halls', [
+            'name' => 'Milad Hall',
+            'complex_id' => $complex->id
+        ]);
+    }
+
+    /** @test **/
+    public function name_is_required()
+    {
+        $this->singIn();
+
+        $complex = Complex::factory()->create([
+            'user_id' => auth()->id()
+        ]);
+
+        $data = [
+            'name' => null,
+            'description' => 'This is a description',
+            'complex_id' => $complex->id
+        ];
+
+        $this->assertDatabaseCount('halls', 0);
+
+        $this->postJson(route('halls.store'), $data)
+            ->assertStatus(400);
+
+        $this->assertDatabaseCount('halls', 0);
+    }
+
+    /** @test **/
+    public function name_must_be_string()
+    {
+        $this->singIn();
+
+        $complex = Complex::factory()->create([
+            'user_id' => auth()->id()
+        ]);
+
+        $data = [
+            'name' => 22,
+            'description' => 'This is a description',
+            'complex_id' => $complex->id
+        ];
+
+        $this->assertDatabaseCount('halls', 0);
+
+        $this->postJson(route('halls.store'), $data)
+            ->assertStatus(400);
+
+        $this->assertDatabaseCount('halls', 0);
+    }
+
+    /** @test **/
+    public function description_must_be_string()
+    {
+        $this->singIn();
+
+        $complex = Complex::factory()->create([
+            'user_id' => auth()->id()
+        ]);
+
+        $data = [
+            'name' => 'Milad Hall',
+            'description' => [],
+            'complex_id' => $complex->id
+        ];
+
+        $this->assertDatabaseCount('halls', 0);
+
+        $this->postJson(route('halls.store'), $data)
+            ->assertStatus(400);
+
+        $this->assertDatabaseCount('halls', 0);
+    }
+
+    /** @test **/
+    public function admin_can_see_all_halls()
+    {
+        $this->withoutExceptionHandling();
+        $this->singIn();
+
+        $halls = Hall::factory()->count(3)->create();
+
+        $this->getJson(route('halls.index'))
+            ->assertJson([
+                "data" => [
+                    [
+                        "name" => $halls[0]['name']
+                    ],
+                    [
+                        "name" => $halls[1]['name']
+                    ]
+                ]
+            ])
+            ->assertStatus(200);
+    }
+
+    /** @test **/
+    public function admin_can_see_a_hall()
+    {
+        $this->withoutExceptionHandling();
+        $this->singIn();
+
+        $hall = Hall::factory()->create();
+
+        $this->getJson(route('halls.show', $hall->id))
+            ->assertJson([
+                "data" => [
+                    "name" => $hall['name']
+                ]
+            ])
+            ->assertStatus(200);
+    }
+}
