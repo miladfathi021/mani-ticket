a8b08d9944097726aed67664f950d0d2cbd78c58
Milad <miladfathi021@gmail.com>
2022-11-07 20:33:11 +0330

    Created seat model with migration

---
+++ app/Models/Seat.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Seat extends Model
+{
+    use HasFactory, SoftDeletes;
+    protected $fillable = ['row', 'column', 'seat_code', 'section_id'];
+}
