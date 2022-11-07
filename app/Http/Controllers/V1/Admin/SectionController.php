5426f81acd2c37525e64200a4b66322e32911f30
Milad <miladfathi021@gmail.com>
2022-11-07 20:31:32 +0330

    Created section controller

---
+++ app/Http/Controllers/V1/Admin/SectionController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\V1\Admin;
+
+use App\Http\Controllers\V1\ApiController;
+use App\Http\Requests\Admin\SectionRequest;
+use App\Services\SectionService;
+
+class SectionController extends ApiController
+{
+    protected SectionService $sectionService;
+
+    /**
+     * SectionController constructor.
+     *
+     * @param \App\Services\SectionService $sectionService
+     */
+    public function __construct(SectionService $sectionService)
+    {
+        $this->sectionService = $sectionService;
+    }
+
+    /**
+     * @param \App\Http\Requests\Admin\SectionRequest $request
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function store(SectionRequest $request) : \Illuminate\Http\JsonResponse
+    {
+        $this->sectionService->create($request->all());
+
+        return $this->response(message: 'Section created successfully!');
+    }
+}
