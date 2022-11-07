f4fdc39693d53f71d45bc4394c22e7d7eb7cabd9
Milad <miladfathi021@gmail.com>
2022-11-07 20:33:30 +0330

    Created section test

---
+++ tests/Feature/Complex/SectionTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Tests\Feature\Complex;
+
+use App\Models\Hall;
+use App\Models\Seat;
+use App\Models\Section;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class SectionTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test **/
+    public function admin_can_create_a_new_section_with_its_seats()
+    {
+        $this->withoutExceptionHandling();
+        $this->singIn();
+
+        $hall = Hall::factory()->create();
+
+        $data = [
+            'sections' => [
+                [
+                    'name' => 'section Vip',
+                    'description' => 'This is a description',
+                    'hall_id' => $hall->id,
+                    'row_count' => 2,
+                    'column_count' => 15,
+                    'row_number_from' => 10,
+                    'column_number_from' => 12,
+                ]
+            ]
+        ];
+
+        $this->assertDatabaseCount('sections', 0);
+        $this->assertDatabaseCount('seats', 0);
+
+        $this->postJson(route('sections.store'), $data)
+            ->assertStatus(200);
+
+        $this->assertDatabaseCount('sections', 1);
+        $this->assertDatabaseCount('seats', 30);
+    }
+}
