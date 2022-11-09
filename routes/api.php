f340cba17d7ab60fc9fbebd4d58ebf496a514b2d
Milad <miladfathi021@gmail.com>
2022-11-09 10:13:19 +0330

    Added event route to api

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::group(['prefix' => '/v1'], function () {
+Route::group(['prefix' => '/v1/admin'], function () {
+    require('Admin/complex.php');
     require('Admin/hall.php');
+    require('Admin/section.php');
+    require('Admin/event.php');
+});
+
+
+Route::group(['prefix' => '/v1'], function () {
+
 });
