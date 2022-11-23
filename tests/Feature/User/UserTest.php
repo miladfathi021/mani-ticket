4ace5c87d95d53a1bc9e87b9ccf8b8e1ce0a9589
Milad <miladfathi021@gmail.com>
2022-11-24 01:37:29 +0330

    Created user test

---
+++ tests/Feature/User/UserTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests\Feature\User;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+    use RefreshDatabase;
+
+
+}
