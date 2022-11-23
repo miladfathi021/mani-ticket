b8b9e77940999cd1b2842804a44abfefc6a1e310
Milad <miladfathi021@gmail.com>
2022-11-24 01:37:01 +0330

    Created development seeder

---
+++ database/seeders/DevelopmentSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DevelopmentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::factory()->create([
+            'name' => 'Milad Fathi',
+            'phone' => '09215420796',
+            'email' => 'miladfathi021@gmail.com'
+        ]);
+    }
+}
