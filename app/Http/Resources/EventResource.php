d13ed09e5e33c0ea206171b1576eb4d916e23144
Milad <miladfathi021@gmail.com>
2022-11-24 01:26:55 +0330

    Created event resources

---
+++ app/Http/Resources/EventResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class EventResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'name' => $this->name,
+            'description' => $this->description,
+            'date_start' => $this->date_start,
+            'time_start' => $this->time_start,
+            'date_end' => $this->date_end,
+            'time_end' => $this->time_end,
+            'complex' => new ComplexResource($this->whenLoaded('complex')),
+            'halls' => new HallCollection($this->whenLoaded('halls')),
+        ];
+    }
+}
