4513c2dc28a26a662907cec715feacbe62b84f2b
Milad <miladfathi021@gmail.com>
2022-11-24 01:28:15 +0330

    Created event service

---
+++ app/Services/EventService.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Services;
+
+use App\Exceptions\DatabaseQueryException;
+use App\Models\Event;
+use App\Repositories\EventRepository\EventRepositoryInterface;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+
+class EventService
+{
+    protected EventRepositoryInterface $eventRepository;
+    private Event $event;
+
+    public function __construct(EventRepositoryInterface $eventRepository)
+    {
+       $this->eventRepository = $eventRepository;
+    }
+
+    /**
+     * @param $data
+     *
+     * @return array
+     * @throws \App\Exceptions\DatabaseQueryException
+     */
+    public function create($data) : array
+    {
+        try {
+            DB::beginTransaction();
+
+            $this->create_event($data);
+            $this->create_event_hall($data['halls']);
+            $this->create_event_seat($data['halls']);
+
+        } catch (\Exception $e) {
+            DB::rollBack();
+            Log::debug($e->getMessage());
+            throw new DatabaseQueryException();
+        }
+
+        DB::commit();
+        return [];
+    }
+
+    /**
+     * @param $data
+     *
+     * @return void
+     */
+    private function create_event($data)
+    {
+        $this->event = $this->eventRepository->create($data);
+    }
+
+    /**
+     * @param $data
+     *
+     * @return void
+     */
+    private function create_event_hall($data)
+    {
+        $this->eventRepository->create_event_hall($this->event, $data);
+    }
+
+    /**
+     * @param $data
+     *
+     * @return void
+     */
+    private function create_event_seat($data)
+    {
+        $this->eventRepository->create_event_seat($this->event, $data);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAll() : mixed
+    {
+        return $this->eventRepository->getAll();
+    }
+
+    public function getById($id)
+    {
+        return $this->eventRepository->getById($id);
+    }
+}
