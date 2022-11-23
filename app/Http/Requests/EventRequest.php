797a27e2b3edbe1c8b3671e3d4b95f74e3ad5b7e
Milad <miladfathi021@gmail.com>
2022-11-24 01:26:40 +0330

    Created event request

---
+++ app/Http/Requests/EventRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class EventRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'bail|string|required',
+            'description' => 'bail|string|nullable',
+            'complex_id' => 'bail|required|exists:complexes,id',
+            'date_start' => 'bail|required|date',
+            'time_start' => 'bail|required|date_format:H:i',
+            'date_end' => 'bail|required|date|after_or_equal:date_start',
+            'time_end' => 'bail|required|date_format:H:i',
+        ];
+    }
+}
