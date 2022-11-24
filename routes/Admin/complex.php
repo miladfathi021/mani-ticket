a3a7ed07929a84cd8d5d2286540a0fa7eba48c98
Milad <miladfathi021@gmail.com>
2022-11-24 11:44:14 +0330

    Added update and destroy routes to complex

--- routes/Admin/complex.php
+++ routes/Admin/complex.php
@@ -8,3 +8,7 @@ Route::get('/complexes', [ComplexController::class, 'index'])
     ->name('complexes.index');
 Route::get('/complexes/{complex}', [ComplexController::class, 'show'])
     ->name('complexes.show');
+Route::patch('/complexes/{complex}', [ComplexController::class, 'update'])
+    ->name('complexes.update');
+Route::delete('/complexes/{complex}', [ComplexController::class, 'destroy'])
+    ->name('complexes.destroy');
