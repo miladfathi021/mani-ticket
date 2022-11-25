462459cd097ae2b53018f47c8af9378c659a18cb
Milad <miladfathi021@gmail.com>
2022-11-25 10:21:54 +0330

    Added update and destroy routes to hall routes

--- routes/Admin/hall.php
+++ routes/Admin/hall.php
@@ -8,3 +8,7 @@ Route::get('/halls', [HallController::class, 'index'])
     ->name('halls.index');
 Route::get('/halls/{hall}', [HallController::class, 'show'])
     ->name('halls.show');
+Route::patch('/halls/{hall}', [HallController::class, 'update'])
+    ->name('halls.update');
+Route::delete('/halls/{hall}', [HallController::class, 'destroy'])
+    ->name('halls.destroy');
