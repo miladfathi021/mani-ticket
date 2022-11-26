40975876c3a14afe03cbde0c4eba2abb53c4aa45
Milad <miladfathi021@gmail.com>
2022-11-26 22:10:15 +0330

    Add update method to section controller

--- app/Http/Controllers/V1/Admin/SectionController.php
+++ app/Http/Controllers/V1/Admin/SectionController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\V1\Admin;
 
 use App\Http\Controllers\V1\ApiController;
 use App\Http\Requests\Admin\SectionRequest;
+use App\Http\Requests\Admin\SectionUpdateRequest;
 use App\Http\Resources\SectoionCollection;
 use App\Http\Resources\SectoionResource;
 use App\Services\SectionService;
@@ -52,11 +53,26 @@ class SectionController extends ApiController
      * @param \App\Http\Requests\Admin\SectionRequest $request
      *
      * @return \Illuminate\Http\JsonResponse
+     * @throws \App\Exceptions\DatabaseQueryException
      */
     public function store(SectionRequest $request) : \Illuminate\Http\JsonResponse
     {
         $this->sectionService->create($request->all());
 
         return $this->response(message: 'Section created successfully!');
+    }
+
+    /**
+     * @param \App\Http\Requests\Admin\SectionUpdateRequest $request
+     * @param                                               $id
+     *
+     * @return \Illuminate\Http\JsonResponse
+     * @throws \App\Exceptions\DatabaseQueryException
+     */
+    public function update(SectionUpdateRequest $request, $id) : \Illuminate\Http\JsonResponse
+    {
+        $this->sectionService->update($request->only(['name', 'description']), $id);
+
+        return $this->response(message: 'Section updated successfully!');
     }
 }
