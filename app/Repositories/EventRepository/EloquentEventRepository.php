9ee9ecbbe04b5d5a9419b24f80b46c4a06048b7a
Milad <miladfathi021@gmail.com>
2022-12-09 21:25:42 +0330

    Added get event seats method

--- app/Repositories/EventRepository/EloquentEventRepository.php
+++ app/Repositories/EventRepository/EloquentEventRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repositories\EventRepository;
 
 use App\Models\Event;
+use App\Models\EventHall;
 use App\Models\Seat;
 use App\Models\Section;
 use App\Repositories\ComplexRepository\ComplexRepositoryInterface;
@@ -98,7 +99,7 @@ class EloquentEventRepository implements EventRepositoryInterface
     /**
      * @return \Illuminate\Database\Eloquent\Collection|array
      */
-    public function getAll() : Collection|array
+    public function get_all() : Collection|array
     {
         return $this->model->with(['complex', 'halls', 'artist'])->get();
     }
@@ -108,7 +109,7 @@ class EloquentEventRepository implements EventRepositoryInterface
      *
      * @return \Illuminate\Database\Eloquent\Model|\Illuminate\Database\Eloquent\Collection|\Illuminate\Database\Query\Builder|array|null
      */
-    public function getById($id) : Model|Collection|Builder|array|null
+    public function get_by_id($id) : Model|Collection|Builder|array|null
     {
         return $this->model->with(['complex', 'halls', 'artist'])->find($id);
     }
@@ -116,8 +117,31 @@ class EloquentEventRepository implements EventRepositoryInterface
     /**
      * @return mixed
      */
-    public function getEventsFromToday() : mixed
+    public function get_todays_events() : mixed
     {
-        return $this->model->where('date_start', Carbon::today()->toDateString())->get();
+        return EventHall::where('date_start', Carbon::today()->toDateString())->get();
+    }
+
+    /**
+     * @param $hallId
+     *
+     * @return mixed
+     */
+    public function get_event_seats($hallId) : mixed
+    {
+        return $this->get_by_id($hallId)->with('seats')->get();
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Collection|array
+     */
+    public function get_all_active_events() : Collection|array
+    {
+        return $this->model
+            ->query()
+            ->whereDate('date_start', '<=', Carbon::today()->format('Y-m-d'))
+            ->whereDate('date_end', '>=', Carbon::today()->format('Y-m-d'))
+            ->with('artist')
+            ->get();
     }
 }
