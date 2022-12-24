93dbd9c21a98cba859c0d55736ed901628b79736
Milad <miladfathi021@gmail.com>
2022-12-24 17:46:02 +0330

    Created event controller

--- app/Http/Controllers/V1/User/EventController.php
+++ app/Http/Controllers/V1/User/EventController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\V1\User;
 
 use App\Http\Controllers\V1\ApiController;
 use App\Http\Resources\EventCollection;
+use App\Http\Resources\EventResource;
 use App\Services\EventService;
 
 class EventController extends ApiController
@@ -24,6 +25,20 @@ class EventController extends ApiController
 
         return $this->response(
             new EventCollection($events)
+        );
+    }
+
+    /**
+     * @param $id
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function show($id) : \Illuminate\Http\JsonResponse
+    {
+        $event = $this->eventService->get_event_with_halls($id);
+
+        return $this->response(
+            new EventResource($event)
         );
     }
 }
