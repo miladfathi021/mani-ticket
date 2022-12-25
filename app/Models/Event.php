df8e3dce7aa0ca582cd0b14fa2b349afaa6cef2b
Milad <miladfathi021@gmail.com>
2022-12-25 18:58:58 +0330

    Added media trait to models

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -2,12 +2,13 @@
 
 namespace App\Models;
 
+use App\Traits\MediaTrait;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Event extends Model
 {
-    use HasFactory;
+    use HasFactory, MediaTrait;
 
     protected $fillable = ['name', 'description', 'artist_id', 'complex_id', 'date_start', 'time_start', 'date_end', 'time_end'];
 
@@ -51,5 +52,13 @@ class Event extends Model
     public function artist() : \Illuminate\Database\Eloquent\Relations\BelongsTo
     {
         return $this->belongsTo(Artist::class, 'artist_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\MorphOne
+     */
+    public function image() : \Illuminate\Database\Eloquent\Relations\MorphOne
+    {
+        return $this->morphOne(Media::class, 'mediable');
     }
 }
