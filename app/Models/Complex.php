071c0026371dd9be223ea7e8d7b4af6de79d2761
Milad <miladfathi021@gmail.com>
2022-12-26 17:22:58 +0330

    Added city id to complex model

--- app/Models/Complex.php
+++ app/Models/Complex.php
@@ -10,7 +10,7 @@ class Complex extends Model
 {
     use HasFactory, SoftDeletes;
 
-    protected $fillable = ['user_id', 'parent_id', 'name', 'address', 'description'];
+    protected $fillable = ['user_id', 'parent_id', 'name', 'address', 'description', 'city_id'];
 
     /**
      * @return \Illuminate\Database\Eloquent\Relations\HasMany
@@ -26,5 +26,13 @@ class Complex extends Model
     public function events() : \Illuminate\Database\Eloquent\Relations\HasMany
     {
         return $this->hasMany(Event::class, 'complex_id', 'id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function city() : \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(City::class);
     }
 }
