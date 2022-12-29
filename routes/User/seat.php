9618cde343a19996af1c4f225e0fe98bfb1ee75a
Milad <miladfathi021@gmail.com>
2022-12-29 19:19:32 +0330

    Added new route to seat routes

--- routes/User/seat.php
+++ routes/User/seat.php
@@ -5,4 +5,6 @@ use App\Http\Controllers\V1\User\SeatController;
 Route::group([], function() {
     Route::get('/seats/{event_hall_id}/{section_id}', [SeatController::class, 'show'])
         ->name('seats.show');
+    Route::put('/seats/{event_hall_id}/status', [SeatController::class, 'changeStatus'])
+        ->name('seats.show');
 });
